8780831f8d3d6ea30395f8049a28f6528863df22
JorshuaG <joshbgearheart@gmail.com>
2021-07-06 13:06:28 -1000

    completed if else background color event

--- js/index.js
+++ js/index.js
@@ -11,13 +11,6 @@ navLinks.forEach(function (nav) {
   });
 });
 
-// .addEventListener("mouseover", function () {
-//   navLinks.style.color = "red";
-// });
-// nav.addEventListener("mouseout", function () {
-//   navLinks.style.color = "";
-// });
-
 const letsGoClick = document.querySelector(".text-content:nth-of-type(1) h2");
 const adventureClick = document.querySelector(
   ".text-content:nth-of-type(2) h2"
@@ -31,8 +24,26 @@ adventureClick.addEventListener("click", function () {
   document.querySelector(".content-destination h2").scrollIntoView();
 });
 
-const pageImgs = document.querySelector("img");
+const navBar = document.querySelector("header");
 
-window.addEventListener("load", function () {
-  pageImgs.addEventListener().fadeIn("slow", "Swing");
+navBar.addEventListener("dblclick", function () {
+  if (navBar.style.backgroundColor === "white") {
+    navBar.style.backgroundColor = "#fff300";
+  } else {
+    navBar.style.backgroundColor = "white";
+  }
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.keyCode === 100) {
+    event.target.style.backgroundColor = "orange";
+  } else {
+    event.target.style.backgroundColor = "";
+  }
+});
+
+const firstImg = document.querySelector(".intro img");
+
+firstImg.addEventListener("wheel", function () {
+  firstImg.display = "none";
 });
